9c1f1beab80fd114d8d281c38df345e4d7e4755b
Jubail Mallick <98705208+Jubail0@users.noreply.github.com>
2025-11-12 14:46:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { use } from "react";
 import { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './index.css';
 import Navbar from "./Components/Navbar.jsx";
 import Footer from "./Components/Footer.jsx";
 import GamePage from "./Pages/GamePage.jsx";
