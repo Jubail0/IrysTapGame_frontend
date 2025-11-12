fa8e0fc5efcd417b4706d8e1b6c2f1e8f899ee17
Jubail Mallick <98705208+Jubail0@users.noreply.github.com>
2025-11-12 13:51:27 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  
-  ],
-})
+  plugins: [react()],
+  server: {
+    port: 5173,
+    host: true,
+  },
+  build: {
+    outDir: "dist",
+  },
+});
